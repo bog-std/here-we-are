636077aa4e9d08eec7971fc0a9d1324192d9b0b8
reillykeele <54957139+reillykeele@users.noreply.github.com>
2020-11-05 18:43:47 -0600

    UI Overhaul

---
+++ bog-std/Assets/_Project/Scripts/Constants/Lookup.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public static class Lookup
+{
+    
+    public static Color Colour(string key)
+    {
+        switch (key.ToUpper())
+        {
+            case "PLAYER": return Color.yellow;
+            case "JORDAN": return Color.cyan;
+            case "WAITER": return Color.magenta;
+            default: return Color.white;
+        }
+    }
+
+}
---
+++ bog-std/Assets/_Project/Scripts/DialogueManager/DialogueScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Assets._Project.Scripts.DialogueManager;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DialogueScript : MonoBehaviour
+{
+    public DialogueManager dialogueManager;
+
+    void Start()
+    {
+        dialogueManager = FindObjectOfType<DialogueManager>();
+
+        Button button = GetComponent<Button>();
+        button.onClick.AddListener(OnClick);
+    }
+
+    void OnClick()
+    {
+        dialogueManager.DisplayNext();
+    }
+}
