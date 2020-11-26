01f892391ecc7806371e8a46492dbbd07924d96f
reillykeele <54957139+reillykeele@users.noreply.github.com>
2020-11-25 19:58:29 -0600

    Hide and Display Phone

---
+++ bog-std/Assets/_Project/Scripts/PhoneHub/PhoneHubController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PhoneHubController : MonoBehaviour
+{
+
+    private Animator _animator;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            _animator.SetBool("IsOpen", true);
+        } else if (Input.GetKeyDown(KeyCode.H))
+        {
+            _animator.SetBool("IsOpen", false);
+        }
+    }
+}
