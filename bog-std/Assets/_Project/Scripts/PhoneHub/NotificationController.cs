7cbb847cef15a06d2630a5ab3967d9a539eb8f4c
reillykeele <54957139+reillykeele@users.noreply.github.com>
2020-11-29 14:46:48 -0600

    Create Notification

---
+++ bog-std/Assets/_Project/Scripts/PhoneHub/NotificationController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Assets._Project.Scripts.DialogueManager;
+using UnityEngine;
+
+public class NotificationController : MonoBehaviour
+{
+    private DialogueManager _dialogueManager;
+    private PhoneHubController _phoneHub;
+
+    public void Awake()
+    {
+        _dialogueManager = FindObjectOfType<DialogueManager>();
+        _phoneHub = FindObjectOfType<PhoneHubController>();
+    }
+
+    public void DisplayNotification() => gameObject.SetActive(true);
+    public void HideNotification() => gameObject.SetActive(false);
+
+    public void ViewPhone_OnClick()
+    {
+        _phoneHub.DisplayMemorySelectionScreen();
+        
+        HideNotification();
+        _phoneHub.DisplayPhone();
+    }
+
+    public void IgnorePhone_OnClick()
+    {
+        HideNotification();
+
+        // Continue with the narrative
+    }
+
+}
